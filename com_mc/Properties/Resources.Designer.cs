203d957fa557d0aff485b2ac5cbd035b401be57a
fb <fbfh2020@163.com>
2020-12-22 09:07:17 +0800

    处理帮助错误问题

--- com_mc/Properties/Resources.Designer.cs
+++ com_mc/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace com_mc.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("lgd_gui.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("com_mc.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
